aba35b28fb405a023309a01a49c048b8fb4c61ea
rommel dalisay <rommelmdalisay@gmail.com>
2025-06-11 09:07:52 +0800

    get items

--- controllers/item.js
+++ controllers/item.js
@@ -1,5 +1,23 @@
 const connection = require('../config/database');
 
-exports.getAllItems = (req, res) => { 
-    
+exports.getAllItems = (req, res) => {
+    const sql = 'SELECT * FROM item i INNER JOIN stock s ON i.item_id = s.item_id';
+
+    try {
+        connection.query(sql, (err, rows, fields) => {
+            if (err instanceof Error) {
+                console.log(err);
+                return;
+            }
+
+            console.log(rows);
+           
+            return res.status(200).json({
+                rows,
+            })
+        });
+    } catch (error) {
+        console.log(error)
+    }
+
 }
