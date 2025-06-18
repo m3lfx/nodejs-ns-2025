1dd5dc2abcdf5a93f9ec24141198d1f8a05a3e62
rommel dalisay <rommelmdalisay@gmail.com>
2025-06-18 11:01:48 +0800

    update profile

--- controllers/user.js
+++ controllers/user.js
@@ -2,16 +2,97 @@ const connection = require('../config/database');
 const bcrypt = require('bcrypt')
 
 const registerUser = async (req, res) => {
+    // {
+    //   "name": "steve",
+    //   "email": "steve@gmail.com",
+    //   "password": "password"
+    // }
+    const { name, password, email, } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userSql = 'INSERT INTO users (name, password, email) VALUES (?, ?, ?)';
+    try {
+        connection.execute(userSql, [name, hashedPassword, email], (err, result) => {
+            if (err instanceof Error) {
+                console.log(err);
+
+                return res.status(401).json({
+                    error: err
+                });
+            }
+
+            return res.status(200).json({
+                success: true,
+                result
+            })
+        });
+    } catch (error) {
+        console.log(error)
+    }
+
+};
+
+const loginUser = (req, res) => {
+    console.log(req.body)
+    const { email, password } = req.body;
+    const sql = 'SELECT id, name, email, password FROM users WHERE email = ? AND deleted_at IS NULL';
+    connection.execute(sql, [email], async (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ error: 'Error logging in', details: err });
+        }
+        if (results.length === 0) {
+            return res.status(401).json({ success: false, message: 'Invalid email or password' });
+        }
+
+        const user = results[0];
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res.status(401).json({ success: false, message: 'Invalid email or password' });
+        }
+
+        // Remove password from response
+        delete user.password;
+
+        return res.status(200).json({
+            success: "welcome back",
+            user: results[0]
+        });
+    });
+};
+
+const updateUser = (req, res) => {
   // {
   //   "name": "steve",
   //   "email": "steve@gmail.com",
   //   "password": "password"
   // }
-  const { name, password, email, } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const userSql = 'INSERT INTO users (name, password, email) VALUES (?, ?, ?)';
+  console.log(req.body, req.file)
+  const { title, fname, lname, addressline, town, zipcode, phone, userId,} = req.body;
+
+  if (req.file) {
+    image = req.file.path.replace(/\\/g, "/");
+  }
+  //     INSERT INTO users(user_id, username, email)
+  //   VALUES(1, 'john_doe', 'john@example.com')
+  // ON DUPLICATE KEY UPDATE email = 'john@example.com';
+  const userSql = `
+  INSERT INTO customer 
+    (title, fname, lname, addressline, town, zipcode, phone, image_path, user_id)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+  ON DUPLICATE KEY UPDATE 
+    title = VALUES(title),
+    fname = VALUES(fname),
+    lname = VALUES(lname),
+    addressline = VALUES(addressline),
+    town = VALUES(town),
+    zipcode = VALUES(zipcode),
+    phone = VALUES(phone),
+    image_path = VALUES(image_path)`;
+    const params = [title, fname, lname, addressline, town, zipcode, phone, image, userId];
+
   try {
-    connection.execute(userSql, [name, hashedPassword, email], (err, result) => {
+    connection.execute(userSql, params, (err, result) => {
       if (err instanceof Error) {
         console.log(err);
 
@@ -31,4 +112,4 @@ const registerUser = async (req, res) => {
 
 };
 
-module.exports = { registerUser, };
+module.exports = { registerUser, loginUser, updateUser};
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../utils/multer')
 
-const {registerUser, } = require('../controllers/user')
+const {registerUser, loginUser, updateUser} = require('../controllers/user')
 
 router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.post('/update-profile', upload.single('image'), updateUser)
 
 module.exports = router;
