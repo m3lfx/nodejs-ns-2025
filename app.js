24b8ca4dd1fdff4f5c4c33f1cab80b2b8e4cc7b3
rommel dalisay <rommelmdalisay@gmail.com>
2025-06-18 09:12:20 +0800

    static images

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const items = require('./routes/item');
 
@@ -8,6 +9,8 @@ app.use(cors())
 
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use('/api/v1', items);
 
